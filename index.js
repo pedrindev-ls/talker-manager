6171875dc9f34946ae7f093a30ea4c3eb2636d7c
Pedrin <pedro515vieira@gmail.com>
2022-06-13 16:23:56 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +15,10 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await fs.readFile('./talker.json');
+  const cTalkers = JSON.parse(talkers);
+  res.status(200).json(cTalkers);
 });
