b9b4b1e63e88a789c4b974b49939ab6d5a2e8b86
Pedrin <pedro515vieira@gmail.com>
2022-06-15 15:49:13 -0300

    :heavy_check_mark: feat: Requisitos 3 e 4

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs/promises');
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,14 +9,19 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
-});
+// const loginValidation = (req, res, next) => {
+//   const { email, password } = req.body;
 
-app.listen(PORT, () => {
-  console.log('Online');
-});
+//   if (!email || email !== /\w+@\w+\.\S+/g) { 
+//     return res.status(404).json({ message: 'O campo "email" é obrigatório' }); 
+//   }
+
+//   if (!password || password < 6) {
+//     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' }); 
+//   }
+
+//   next();
+// };
 
 app.get('/talker', async (_req, res) => {
   const talkers = await fs.readFile('./talker.json');
@@ -34,4 +40,34 @@ app.get('/talker/:id', async (req, res) => {
   }
 
   res.status(200).send(itemSelected);
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const generatedToken = crypto.randomBytes(8).toString('hex');
+  const EMAILREGEX = /\w+@\w+\.\S+/g;
+
+  if (!email) { 
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' }); 
+  }
+  if (!EMAILREGEX.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); 
+  }
+
+  return res.status(200).json({ token: generatedToken });
+  });
+
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.status(HTTP_OK_STATUS).send();
+});
+
+app.listen(PORT, () => {
+  console.log('Online');
 });
